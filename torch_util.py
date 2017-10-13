fbaabbcfa9ea8a568d7c52ec02c66151ba0b436b
Yixin Nie <nyixin318@gmail.com>
2017-10-13 16:57:16 -0400

    Update some stuff in util including some doc changes.

--- torch_util.py
+++ torch_util.py
@@ -6,44 +6,53 @@ import numpy as np
 import functools
 
 
-def pad_1d(t, pad_l):
-    l = t.size(0)
+def pad_1d(seq, pad_l):
+    """
+    The seq is a sequence having shape [T, ..]. Note: The seq contains only one instance. This is not batched.
+    
+    :param seq:  Input sequence with shape [T, ...]
+    :param pad_l: The required pad_length.
+    :return:  Output sequence will have shape [Pad_L, ...]
+    """
+    l = seq.size(0)
     if l >= pad_l:
-        return t
+        return seq[:pad_l, ]  # Truncate the length if the length is bigger than required padded_length.
     else:
-        pad_seq = Variable(t.data.new(pad_l - l, *t.size()[1:]).zero_())
-        return torch.cat([t, pad_seq], dim=0)
+        pad_seq = Variable(seq.data.new(pad_l - l, *seq.size()[1:]).zero_())  # Requires_grad is False
+        return torch.cat([seq, pad_seq], dim=0)
 
 
-def pad(t, length, batch_first=False):
+def pad(seqs, length, batch_first=True):
+    #TODO The method seems useless to me. Delete?
     """
     Padding the sequence to a fixed length.
-    :param t: [B * T * D] or [B * T] if batch_first else [T * B * D] or [T * B]
+    
+    :param seqs: [B, T, D] or [B, T] if batch_first else [T, B * D] or [T, B]
     :param length: [B]
     :param batch_first:
     :return:
     """
     if batch_first:
         # [B * T * D]
-        if length <= t.size(1):
-            return t
+        if length <= seqs.size(1):
+            return seqs
         else:
-            batch_size = t.size(0)
-            pad_seq = Variable(t.data.new(batch_size, length - t.size(1), *t.size()[2:]).zero_())
+            batch_size = seqs.size(0)
+            pad_seq = Variable(seqs.data.new(batch_size, length - seqs.size(1), *seqs.size()[2:]).zero_())
             # [B * T * D]
-            return torch.cat([t, pad_seq], dim=1)
+            return torch.cat([seqs, pad_seq], dim=1)
     else:
         # [T * B * D]
-        if length <= t.size(0):
-            return t
+        if length <= seqs.size(0):
+            return seqs
         else:
-            return torch.cat([t, Variable(t.data.new(length - t.size(0), *t.size()[1:]).zero_())])
+            return torch.cat([seqs, Variable(seqs.data.new(length - seqs.size(0), *seqs.size()[1:]).zero_())])
 
 
 def batch_first2time_first(inputs):
     """
     Convert input from batch_first to time_first:
-    [B * T * D] -> [T * B * D]
+    [B, T, D] -> [T, B, D]
     
     :param inputs:
     :return:
@@ -54,12 +63,11 @@ def batch_first2time_first(inputs):
 def time_first2batch_first(inputs):
     """
     Convert input from batch_first to time_first:
-    [T * B * D] -> [B * T * D] 
+    [T, B, D] -> [B, T, D] 
     
     :param inputs:
     :return:
     """
-
     return torch.transpose(inputs, 0, 1)
 
 
@@ -68,11 +76,11 @@ def get_state_shape(rnn: nn.RNN, batch_size, bidirectional=False):
     Return the state shape of a given RNN. This is helpful when you want to create a init state for RNN.
 
     Example:
-    h0 = Variable(src_seq_p.data.new(*get_state_shape(self.encoder, 3, bidirectional)).zero_())
+    c0 = h0 = Variable(src_seq_p.data.new(*get_state_shape([your rnn], 3, bidirectional)).zero_())
     
-    :param rnn: 
-    :param batch_size: 
-    :param bidirectional: 
+    :param rnn: nn.LSTM, nn.GRU or subclass of nn.RNN
+    :param batch_size:  
+    :param bidirectional:  
     :return: 
     """
     if bidirectional:
@@ -81,28 +89,31 @@ def get_state_shape(rnn: nn.RNN, batch_size, bidirectional=False):
         return rnn.num_layers, batch_size, rnn.hidden_size
 
 
-def pack_list_sequence(inputs, l, batch_first=False):
+def pack_list_sequence(inputs, l, max_l=None, batch_first=True):
     """
-    Pack a batch of Tensor into one Tensor.
+    Pack a batch of Tensor into one Tensor with max_length.
     :param inputs: 
     :param l: 
+    :param max_l: The max_length of the packed sequence.
+    :param batch_first: 
     :return: 
     """
     batch_list = []
-    max_l = max(list(l))
+    max_l = max(list(l)) if not max_l else max_l
     batch_size = len(inputs)
 
     for b_i in range(batch_size):
-        batch_list.append(pad(inputs[b_i], max_l))
+        batch_list.append(pad_1d(inputs[b_i], max_l))
     pack_batch_list = torch.stack(batch_list, dim=1) if not batch_first \
         else torch.stack(batch_list, dim=0)
     return pack_batch_list
 
 
-def pack_for_rnn_seq(inputs, lengths, batch_first=False):
+def pack_for_rnn_seq(inputs, lengths, batch_first=True):
     """
-    :param inputs: [T * B * D] 
+    :param inputs: Shape of the input should be [B, T, D] if batch_first else [T, B, D].
     :param lengths:  [B]
+    :param batch_first: 
     :return: 
     """
     if not batch_first:
@@ -152,7 +163,7 @@ def pack_for_rnn_seq(inputs, lengths, batch_first=False):
         return packed_seq, reverse_indices
 
 
-def unpack_from_rnn_seq(packed_seq, reverse_indices, batch_first=False):
+def unpack_from_rnn_seq(packed_seq, reverse_indices, batch_first=True):
     unpacked_seq, _ = nn.utils.rnn.pad_packed_sequence(packed_seq, batch_first=batch_first)
     s_inputs_list = []
 
@@ -167,7 +178,6 @@ def unpack_from_rnn_seq(packed_seq, reverse_indices, batch_first=False):
 
 
 def auto_rnn(rnn: nn.RNN, seqs, lengths, batch_first=True):
-
     batch_size = seqs.size(0) if batch_first else seqs.size(1)
     state_shape = get_state_shape(rnn, batch_size, rnn.bidirectional)
 
@@ -180,13 +190,11 @@ def auto_rnn(rnn: nn.RNN, seqs, lengths, batch_first=True):
     return output
 
 
-def pack_seqence_for_linear(inputs, lengths, batch_first=True):
+def pack_sequence_for_linear(inputs, lengths, batch_first=True):
     """
-    :param inputs: [B * T * D] if batch_first 
+    :param inputs: [B, T, D] if batch_first 
     :param lengths:  [B]
     :param batch_first:  
-    :param partition:  this is the new batch_size for parallel matrix process.
-    :param chuck: partition the output into equal size chucks
     :return: 
     """
     batch_list = []
@@ -198,7 +206,6 @@ def pack_seqence_for_linear(inputs, lengths, batch_first=True):
         #     return list(torch.chunk(packed_sequence, chuck, dim=0))
         # else:
         return packed_sequence
-
     else:
         raise NotImplemented()
 
@@ -211,7 +218,7 @@ def chucked_forward(inputs, net, chuck=None):
         return torch.cat(output_list, dim=0)
 
 
-def unpack_seqence_for_linear(inputs, lengths, batch_first=True):
+def unpack_sequence_for_linear(inputs, lengths, batch_first=True):
     batch_list = []
     max_l = max(lengths)
 
@@ -232,29 +239,34 @@ def unpack_seqence_for_linear(inputs, lengths, batch_first=True):
 
 def seq2seq_cross_entropy(logits, label, l, chuck=None):
     """
-    :param logits: [exB * V] : exB = sum(l)
+    :param logits: [exB, V] : exB = sum(l)
     :param label: [B] : a batch of Label
     :param l: [B] : a batch of LongTensor indicating the lengths of each inputs
     :param chuck: Number of chuck to process
     :return: A loss value
     """
-    packed_label = pack_seqence_for_linear(label, l)
-    cross_entropy_losss = functools.partial(F.cross_entropy, size_average=False)
+    packed_label = pack_sequence_for_linear(label, l)
+    cross_entropy_loss = functools.partial(F.cross_entropy, size_average=False)
     total = sum(l)
+
+    assert total == logits.size(0) or packed_label.size(0) == logits.size(0),\
+        "logits length mismatch with label length."
+
     if chuck:
         logits_losses = 0
         for x, y in zip(torch.chunk(logits, chuck, dim=0), torch.chunk(packed_label, chuck, dim=0)):
-            logits_losses += cross_entropy_losss(x, y)
+            logits_losses += cross_entropy_loss(x, y)
         return logits_losses * (1 / total)
     else:
-        return cross_entropy_losss(logits, packed_label) * (1 / total)
+        return cross_entropy_loss(logits, packed_label) * (1 / total)
 
 
 def max_along_time(inputs, lengths, list_in=False):
     """
-    :param inputs: [T * B * D] 
+    :param inputs: [B, T, D] 
     :param lengths:  [B]
     :return: [B * D] max_along_time
+    :param list_in: 
     """
     ls = list(lengths)
 
